8f281f2784e100e7d804cf5a3fb5dfd8f7a3d286
Nikhil Aditya <nik333_adi@yahoo.co.in>
2019-11-21 10:02:05 +0530

    please work

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
-docker build -t niksadi/multi-client:latest -t niksadi/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t niksadi/multi-server:latest -t niksadi/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t niksadi/multi-worker:latest -t niksadi/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t niksadi/multi-client -f ./client/Dockerfile ./client
+docker build -t niksadi/multi-server -f ./server/Dockerfile ./server
+docker build -t niksadi/multi-worker -f ./worker/Dockerfile ./worker
 
-docker push niksadi/multi-client:latest
-docker push niksadi/multi-server:latest
-docker push niksadi/multi-worker:latest
+docker push niksadi/multi-client
+docker push niksadi/multi-server
+docker push niksadi/multi-worker
 
-docker push niksadi/multi-client:$SHA
-docker push niksadi/multi-server:$SHA
-docker push niksadi/multi-worker:$SHA
+docker push niksadi/multi-client
+docker push niksadi/multi-server
+docker push niksadi/multi-worker
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=niksadi/multi-server:$SHA
-kubectl set image deployments/client-deployment client=niksadi/multi-client:$SHA
-kubectl set image deployments/worker-deployment worker=niksadi/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=niksadi/multi-server
+kubectl set image deployments/client-deployment client=niksadi/multi-client
+kubectl set image deployments/worker-deployment worker=niksadi/multi-worker
