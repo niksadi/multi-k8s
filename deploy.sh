07c4ae9c5a483aea6e50897de99b02f9b8db4f5c
Nikhil Aditya <nik333_adi@yahoo.co.in>
2019-11-21 09:44:43 +0530

    make it work

--- deploy.sh
+++ deploy.sh
@@ -14,3 +14,4 @@ kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=niksadi/multi-server:$SHA
 kubectl set image deployments/client-deployment client=niksadi/multi-client:$SHA
 kubectl set image deployments/worker-deployment worker=niksadi/multi-worker:$SHA
+
